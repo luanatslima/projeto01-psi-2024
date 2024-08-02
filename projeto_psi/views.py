138b7df0946aba56931b57ecd1d3ca6b3422620d
Luana <luanatayna2007@gmail.com>
2024-08-02 10:21:19 -0300

    Novo commit

--- projeto_psi/views.py
+++ projeto_psi/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def jogadoras(request):
+    jogadoras = [
+        {"nome": "Marta", "idade": "40", "posição": "", "local de nascimento": "", "foto": ""}
+        {"nome": "Antônia", "idade": "", "posição": "", "local de nascimento": "", "foto": ""}
+        {"nome": "", "idade": "", "posição": "", "local de nascimento": "", "foto": ""}
+        {"nome": "", "idade": "", "posição": "", "local de nascimento": "", "foto": ""}
+    ]
+    context = {
+        "jogadoras" : jogadoras
+    }
+    return render(request, "jogadoras.html", context)
+
+
+def sobre(request):
+    return render(request, "sobre.html")

9a4da81c0b677d6316201c6c432d60fcb7ddd3a4
luana <luanatayna2007@gmail>
2024-08-07 15:23:06 -0300

    Novo commit

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include('projeto_psi.urls')),
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
